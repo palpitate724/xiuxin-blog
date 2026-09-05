116fcc2c522888fef63068ca818c1255f56c28a9
palpitate724 <suyang67@outlook.com>
2026-09-05 10:37:42 +0800

    0.1
    服务端框架搭建

---
+++ blog_spring/blog_domain/src/test/java/com/example/blog_donain/BlogDonainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_donain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDonainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
